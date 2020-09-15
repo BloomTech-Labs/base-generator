f399a88930b4533877e11eac51edd0bbaa4f01cc
Jake Mallory <4095+tinomen@users.noreply.github.com>
2020-09-15 13:18:50 -0600

    add functions to create common options/prompts

--- baseGenerator.js
+++ baseGenerator.js
@@ -13,6 +13,66 @@ module.exports = class extends Generator {
     this.option(name, optionOpts);
     this.prompts.push(promptOpts);
   }
+
+  _makeProgram() {
+    this._makePromptOption(
+      'program',
+      {
+        type: 'list',
+        message: 'What is the project type?',
+        choices: [
+          {
+            name: 'Buid Weeks',
+            value: 'bw',
+          },
+          {
+            name: 'Labs',
+            value: 'labs',
+          },
+        ],
+        default: 'labs',
+        store: true,
+      },
+      {
+        type: String,
+        alias: 'p',
+        desc: 'Which program will this be used for: "bw" or "labs"',
+      }
+    );
+  }
+
+  _makeHasDS() {
+    this._makePromptOption(
+      'hasDS',
+      {
+        type: 'confirm',
+        message: 'Does your project have Data Science team members?',
+        default: false,
+      },
+      {
+        type: (val) => { return (val==='false' ? false : true)},
+        alias: 'd',
+        desc: 'project has DS team members',
+      }
+    );
+  }
+
+  _makeRepoUrl() {
+    this._makePromptOption(
+      'repoUrl',
+      {
+        type: 'input',
+        message: 'Enter your Github repo HTTPS git url (https://github.com/user/repo-name)',
+        default: '',
+        store: true,
+      },
+      {
+        type: String,
+        alias: 'r',
+        desc: 'The Github repo HTTPS git url. eg, https://github.com/lambda-school-labs/labsNN-productA-teamB-fe',
+      }
+    );
+  }
   
   _removePrompt(name) {
     this.prompts.splice(
